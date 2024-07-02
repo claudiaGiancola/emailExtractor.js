b6a02ad92347212c5ffe15790607eddbe6f2a225
Claudia Giancola <claudia.giancola@softwire.com>
2024-07-02 15:31:55 +0100

    Dictionaries of 10 most common emails added

--- emailExtraction.js
+++ emailExtraction.js
@@ -1,13 +1,9 @@
-// const testLine = "sforequarters Horlivka's Wideners footings canthooks venerations interdiffused belch softwire.comvenatical zocalo faller eyewaters Costners tickseed dismountable chauntry syndetic outlook.com analise@yahoo.com whammys collie unalphabetized regaling boffin neologist swi.re Yizkors Miraloma smutty nondescriptive zoosperm campfires bridals brainchildren funks bigos hotmail.fr maegan_alyce@my-email.net weakhanded unpeerable bullheaded waspishness commutes yahoo.com hotmail.fr registrable tangiblenesses Switzers clearest Victorianizing brunhilda_zullo@techswitch.co.uk womanpost nissie_elconin@gmail.com felspathoid gmail.com guildhalls pardi swellfishes hotmail.com Casaubon estimations wirelike mirelle.taub@outlook.com reafforestation dannebrog hotmail.com schipperkes communicably anthologizing extractively softwire.come t.net perichondrial overindebtednesses densifier Glenmonts disattuning clit frisker hydrid gurges coreign";
-
-
 var fs = require("fs");
 
 fs.readFile("./test.txt", "utf-8",function read(err, data) {
   if (err) {
       throw err;
   }
-  // const content  = data;
 
   // Invoke the next step here however you like
   //console.log(content);   // Put all of the code here (not the best solution)
@@ -22,24 +18,30 @@ let counter = 0;
 const matches = text.match(/softwire.com/g);
 console.log(matches.length);
 
-
-/* 
-for(let i = 0; i < text.length; i++) {
-
-  if (text.substring(i, (i + 12)) === "softwire.com") {
-    counter += 1;
-    }
-
-}
-
-console.log(counter);
-*/
 }
 
 function getDomains(text) {
 
   const domains = text.match(/@[A-Z0-9.'_%+-]*/gi);
 
-  console.log(domains);
+  const domainObj = {};
+
+  for (let domain of domains) {
+    // console.log(domain);
+    domainObj[domain] = domainObj[domain] + 1 || 1;
+    
+  }
+
+  let commonDom = [];
+
+  for (let domain in domainObj) {
+    commonDom.push([domain, domainObj[domain]]);
+}
+
+  commonDom.sort(function(a, b) {
+    return b[1] - a[1];
+  });
+
+  console.log(commonDom.slice(0, 10));
 
 }
