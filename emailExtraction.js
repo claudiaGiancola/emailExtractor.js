2c5d52572df7ae088f24dd16dbf873525e520d57
Claudia Giancola <claudia.giancola@softwire.com>
2024-07-02 11:49:03 +0100

    For loop test working fine, with text as a varible

---
+++ emailExtraction.js
@@ -0,0 +1,17 @@
+const text = "forequarters Horlivka's Wideners footings canthooks venerations interdiffused belch softwire.comvenatical zocalo faller eyewaters Costners tickseed dismountable chauntry syndetic outlook.com analise@yahoo.com whammys collie unalphabetized regaling boffin neologist swi.re Yizkors Miraloma smutty nondescriptive zoosperm campfires bridals brainchildren funks bigos hotmail.fr maegan_alyce@my-email.net weakhanded unpeerable bullheaded waspishness commutes yahoo.com hotmail.fr registrable tangiblenesses Switzers clearest Victorianizing brunhilda_zullo@techswitch.co.uk womanpost nissie_elconin@gmail.com felspathoid gmail.com guildhalls pardi swellfishes hotmail.com Casaubon estimations wirelike mirelle.taub@outlook.com reafforestation dannebrog hotmail.com schipperkes communicably anthologizing extractively softwire.comet.net perichondrial overindebtednesses densifier Glenmonts disattuning clit frisker hydrid gurges coreign";
+
+// const fs = require("fs");
+
+// fs.readFile("test");
+
+let counter = 0
+ 
+for(let i = 0; i < text.length; i++) {
+
+  if (text.substring(i, (i + 12)) === "softwire.com") {
+    counter += 1;
+    }
+
+}
+
+console.log(counter);
